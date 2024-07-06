b41eca227d901ffd2be0061e13c18d79d0d0cfdf
Anchik17 <151466304+Anchik17@users.noreply.github.com>
2024-07-06 18:48:58 +0200

    react-hw-2-done

---
+++ src/components/Options/Options.jsx
@@ -0,0 +1,12 @@
+const Options = ({ onFeedback, total, reset }) => {
+  return (
+    <div>
+      <button onClick={() => onFeedback("good")}>Good</button>
+      <button onClick={() => onFeedback("neutral")}>Neutral</button>
+      <button onClick={() => onFeedback("bad")}>Bad</button>
+      {total > 0 && <button onClick={reset}>Reset</button>}
+    </div>
+  );
+};
+
+export default Options;
